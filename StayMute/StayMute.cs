d1aa55658f8dbc2dae199ea2323e1e288d6f6d93
Elly <git@ellyvr.dev>
2022-01-25 19:44:35 -0600

    Derp and a smol cleanup

--- StayMute/StayMute.cs
+++ StayMute/StayMute.cs
@@ -10,11 +10,11 @@ namespace StayMute
         public override void OnApplicationStart()
         {
             _StayMute = MelonPreferences.CreateCategory("StayMute", "StayMute");
-            _Enabled = _StayMute.CreateEntry("Enabled", false, "StayMute_Enabled", "Keep yourself from unmuting");
+            _Enabled = _StayMute.CreateEntry("Enabled", false, "StayMute", "Keep yourself from unmuting");
             //Thanks to DDAkebono for helping me find this. 
             DefaultTalkController.field_Private_Static_Action_0 += new Action(() =>
             {
-                if (_Enabled.Value) return;
+                if (!_Enabled.Value) return;
                 DefaultTalkController.Method_Public_Static_Void_Boolean_0(true);
             });
             Logger.Msg("Started Successfully");
